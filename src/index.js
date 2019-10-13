31909ab5ccd52564441d05e0ebef9109bd2d645b
abuna1985 <adamjabundis@gmail.com>
2019-10-12 19:55:52 -0700

    Updated increment and decrement button functionality. imported normalize.css and syle.css.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-const $ = require("jquery");
+import { library, dom, counter } from "@fortawesome/fontawesome-svg-core";
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
+import { faMinus } from "@fortawesome/free-solid-svg-icons/faMinus";
+import 'normalize.css';
+import './style.css';
 
-$("#target").html("hello world!");
+library.add(faPlus, faMinus);
+dom.watch();
+
+let $counter = document.getElementById("counter");
+
+$counter.innerHTML = 0;
+
+document.addEventListener('click', (event) => {
+  
+  if (event.target.closest('.btn--add')) {
+    event.preventDefault();
+    $counter.innerHTML = incrementCounter(parseInt($counter.innerHTML));
+  }
+
+  if (event.target.closest('.btn--subtract')) {
+    event.preventDefault();
+    $counter.innerHTML = decrementCounter($counter.innerHTML);
+  }
+
+  return false;
+}, false)
+
+const incrementCounter = currentVal => {
+  return ++currentVal;
+}
+
+const decrementCounter = currentVal => {
+  if (currentVal > 0) {
+    return --currentVal;
+  }
+
+  return 0;
+}
